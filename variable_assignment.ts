06f0b650844e7d2e8dff81ea347c2498fb6a445b
muazam mughal <104276759+MuazamMughal@users.noreply.github.com>
2023-03-20 02:26:14 +0500

    initial commit

---
+++ variable_assignment.ts
@@ -0,0 +1,17 @@
+/* there are two techniques of variable type assignment is ts
+one is explicit variable type assignment and second one is implicit
+variable type assingnment*/
+
+// the first one Explicit type is given below
+let myName : string ="Muazam Mughal";
+console.log(myName);
+//in Explicit type we declear data type specfically
+
+
+// Now the second type is Implicit type assignment
+
+let myname ="Muazam Mughal";
+console.log(myname)
+
+//in implicit type we are not specifies the type of variable and
+//lend it to ts to judge type own its own
